572533087c7ff51bb91a04b69307628a5e889625
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-09-09 17:58:14 -0500

    Created and registered ArticlesController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,3 +3,5 @@
 Route::get('/', 'PagesController@welcome');
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
+
+Route::get('articles', 'ArticlesController@index');
