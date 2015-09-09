2714f6aae432753372162de700aed9364051b816
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-09-09 18:05:14 -0500

    Created ArticlesController@index method

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Article;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class ArticlesController extends Controller
 {
-    //
+  public function index()
+  {
+    $articles = Article::all();
+
+    return $articles;
+  }
 }
