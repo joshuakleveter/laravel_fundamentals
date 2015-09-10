a95bf435a8bc363fc79e72436ad160a57c638ece
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-09-10 16:51:02 -0500

    Added 'create' page to site

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Request;
 
 use App\Article;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 
 class ArticlesController extends Controller
 {
   public function index()
   {
-    $articles = Article::all();
+    $articles = Article::latest('published_at')->get();
 
     return view('articles.index', compact('articles'));
   }
@@ -22,5 +23,20 @@ class ArticlesController extends Controller
     $article = Article::findOrFail($id);
 
     return view('articles.show', compact('article'));
+  }
+
+  public function create()
+  {
+    return view('articles.create');
+  }
+
+  public function store()
+  {
+    $input = Request::all();
+    $input['published_at'] = Carbon::now();
+
+    Article::create($input);
+
+    return redirect('articles');
   }
 }
