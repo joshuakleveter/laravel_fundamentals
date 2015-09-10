9d7ca82074b1a3599226ded0a5caad77e4b08332
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-09-10 18:01:03 -0500

    Commented form inputs

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -5,11 +5,13 @@
   <hr>
 
   {!! Form::open(['url'=>'articles']) !!}
+    <!-- title form input-->
     <div class="form-group">
       {!! Form::label('title', 'Title:') !!}
       {!! Form::text('title', null, ['class'=>'form-control']) !!}
     </div>
 
+    <!-- body form input -->
     <div class="form-group">
       {!! Form::label('body', 'Body:') !!}
       {!! Form::textarea('body', null, ['class'=>'form-control']) !!}
@@ -21,6 +23,7 @@
       {{!! Form::text('published_at', null, ['class'=>'form-control']) !!}}
     </div>
 
+    <!-- add article form input -->
     <div class="form-group">
       {!! Form::submit('Add Article', ['class'=>'btn btn-primary form-control']) !!}
     </div>
