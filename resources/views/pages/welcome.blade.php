450dd0b5c15701dbdb004d3118fc1655452431fa
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-09-15 05:39:23 -0500

    Added nav links to welcome view

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -4,4 +4,10 @@
 
   <h1>Welcome</h1>
 
+  <ul>
+      <li><a href="about">About Us</a></li>
+      <li><a href="contact">Contact</a></li>
+      <li><a href="articles">Blog</a></li>
+  </ul>
+
 @stop
