a05b94832ee0c566eee0a306742ad479d38aae8b
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-09-17 06:23:28 -0500

    Changed CreateArticlesRequest to ArticlesRequest

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Article;
 use App\Http\Requests;
-use App\Http\Requests\CreateArticleRequest;
+use App\Http\Requests\ArticleRequest;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -31,7 +31,7 @@ class ArticlesController extends Controller
     return view('articles.create');
   }
 
-  public function store(CreateArticleRequest $request)
+  public function store(ArticleRequest $request)
   {
     Article::create($request->all());
 
@@ -44,7 +44,7 @@ class ArticlesController extends Controller
     return view('articles.edit', compact('article'));
   }
 
-  public function update($id, Request $request)
+  public function update($id, ArticleRequest $request)
   {
     $article = Article::findOrFail($id);
     $article->update($request->all());
