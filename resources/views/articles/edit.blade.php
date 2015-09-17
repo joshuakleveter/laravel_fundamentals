48afd2f4d96cc5fbc5afed6986d1bff73d11687b
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-09-17 05:49:12 -0500

    Added edit form

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -2,4 +2,30 @@
 
 @section('content')
     <h1>Edit: {{ $article->title }}</h1>
+
+    {!! Form::open(['method'=>'PATCH', 'action'=>['ArticlesController@update', $article->id]]) !!}
+      <!-- title form input-->
+      <div class="form-group">
+        {!! Form::label('title', 'Title:') !!}
+        {!! Form::text('title', null, ['class'=>'form-control']) !!}
+      </div>
+
+      <!-- body form input -->
+      <div class="form-group">
+        {!! Form::label('body', 'Body:') !!}
+        {!! Form::textarea('body', null, ['class'=>'form-control']) !!}
+      </div>
+
+      <!-- published_at form input -->
+      <div class="form-group">
+        {!! Form::label('published_at', 'Publish On:') !!}
+        {!! Form::input('date', 'published_at', date('Y-m-d'), ['class'=>'form-control']) !!}
+      </div>
+
+      <!-- add article form input -->
+      <div class="form-group">
+        {!! Form::submit('Add Article', ['class'=>'btn btn-primary form-control']) !!}
+      </div>
+    {!! Form::close() !!}
+
 @endsection
