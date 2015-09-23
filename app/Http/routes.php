359b6fa387ae3c765a123544bba3e1f780b6c858
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-09-22 19:00:50 -0500

    Added authentication routes and default auth views

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,3 +5,8 @@ Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 
 Route::resource('articles', 'ArticlesController');
+
+Route::controllers([
+  'auth' => 'Auth\AuthController',
+  'password' => 'Auth\PasswordController',
+]);
